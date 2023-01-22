6b00b35cadf28b681db3815cba048d1c173cc64a
pkamibayashi <paulo_kamibayashi@hotmail.com>
2023-01-22 14:21:37 -0300

    script change bestseller mvp done

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-"use strict";
+function enterprise() {
+  const planos = document.querySelector(".premium");
+  const bestSeller = document.createElement("p");
+  bestSeller.classList.add("bestseller");
+  bestSeller.innerText = "BESTSELLER";
+  planos.insertAdjacentElement("afterbegin", bestSeller);
+  const planosStandard = document.querySelector(".standard");
+  const planoStandardChildren = planosStandard.children[0].innerText;
+  if (planoStandardChildren === "BESTSELLER") {
+    planosStandard.children[0].remove();
+  }
+}
+function personal() {
+  const planos = document.querySelector(".standard");
+  const bestSeller = document.createElement("p");
+  bestSeller.classList.add("bestseller");
+  bestSeller.innerText = "BESTSELLER";
+  planos.insertAdjacentElement("afterbegin", bestSeller);
+  const planosPremium = document.querySelector(".premium");
 
-const personalBTN = document.querySelector(".personal");
-const enterpriseBTN = document.querySelector(".enterprise");
+  const planosPremiumChildren = planosPremium.children[0].innerText;
+
+  if (planosPremiumChildren === "BESTSELLER") {
+    planosPremium.children[0].remove();
+  }
+}
+
+function removeBestBtn() {}
+
+window.addEventListener("load", () => {
+  const personalBtn = document.getElementById("personal");
+  const enterpriseBtn = document.getElementById("enterprise");
+
+  enterpriseBtn.addEventListener("click", enterprise);
+  personalBtn.addEventListener("click", personal);
+});
